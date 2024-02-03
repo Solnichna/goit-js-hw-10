a4948aac79d9b9f331f7cd73db78397d18cb796f
Solnichna <143127001+Solnichna@users.noreply.github.com>
2024-02-03 18:56:15 +0100

    part3

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,32 +1,34 @@
-function createPromise(delay, state) {
-  return new Promise((resolve, reject) => {
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+document.getElementById('notification-form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delayInput = document.querySelector('input[name="delay"]');
+  const stateRadio = document.querySelector('input[name="state"]:checked');
+
+  const delay = parseInt(delayInput.value);
+
+  const notificationPromise = new Promise((resolve, reject) => {
+
     setTimeout(() => {
-      if (state === 'fulfilled') {
+      if (stateRadio.value === 'fulfilled') {
         resolve(delay);
       } else {
         reject(delay);
       }
     }, delay);
   });
-}
 
-document.getElementById('promiseForm').addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  const delay = parseInt(this.elements.delay.value);
-  const state = this.elements.state.value;
-
-  createPromise(delay, state)
-    .then((result) => {
-      iziToast.success({
-        title: 'Fulfilled promise',
-        message: `✅ Fulfilled promise in ${result}ms`,
-      });
-    })
-    .catch((result) => {
-      iziToast.error({
-        title: 'Rejected promise',
-        message: `❌ Rejected promise in ${result}ms`,
-      });
+  notificationPromise.then((result) => {
+    iziToast.success({
+      title: 'Fulfilled promise',
+      message: `✅ Fulfilled promise in ${result}ms`,
     });
+  }).catch((result) => {
+    iziToast.error({
+      title: 'Rejected promise',
+      message: `❌ Rejected promise in ${result}ms`,
+    });
+  });
 });
